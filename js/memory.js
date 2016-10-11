89a066b8863351a0b90db392e248650f40eb53ab
Andrew Malkin and Ryan Vinyard <andrew@>
2016-10-10 17:00:10 -0700

    fix a typo

--- js/memory.js
+++ js/memory.js
@@ -3,14 +3,9 @@ fuction Memory(name) {
   this.number = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8', '9', '9', '10', '10'];
   this.cardFlip = false;
   this.previousCard = -2;
-}
+};
 
-Memory.prototype.firstCardFlip = function (card1) {
-  var
-}
-
-Memory.prototype.secondCardFlip = function (card2) {
+Memory.prototype.randomizeNumber = function() {
 
 }
-
 exports.memoryModule = Memory;
